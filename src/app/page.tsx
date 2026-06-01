a622f436392d420a9f2f7bf063b494954c511033
siddiquah <siddiquahanjum111@gmail.com>
2026-06-02 04:19:03 +0530

    init: made product list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,65 @@
-import Image from "next/image";
+import { products } from "@/data/products";
 
 export default function Home() {
   return (
-    <div>
-      <h1>hui</h1>
+    <div className="max-w-7xl mx-auto px-6 py-10">
+      <h1 className="text-5xl font-bold mb-10">
+        Music Store
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="
+              bg-white
+              p-4
+              rounded-xl
+              shadow-md
+              hover:shadow-xl
+              hover:-translate-y-1
+              transition
+              duration-300
+              flex
+              flex-col
+            "
+          >
+            <img
+              src={product.image}
+              alt={product.name}
+              className="
+                w-full
+                h-104
+                object-cover
+                rounded-lg
+              "
+            />
+
+            <h2 className="font-semibold text-lg mt-4">
+              {product.name}
+            </h2>
+
+            <p className="mt-2 text-2xl font-bold">
+              ₹{product.price.toLocaleString()}
+            </p>
+
+            <button
+              className="
+                mt-auto
+                w-full
+                bg-slate-900
+                text-white
+                py-3
+                rounded-lg
+                hover:bg-slate-700
+                transition
+              "
+            >
+              Add to Cart
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/data/products.ts
@@ -0,0 +1,21 @@
+export const products = [
+    {
+        id: 1,
+        name: "Sonor AQ2 Studio Set 5 Piece Acoustic Drumkit",
+        price: 30000,
+        image: "https://thememusic.in/wp-content/uploads/2023/05/GL-30.png"
+    },
+    {
+        id: 2,
+        name: "Cort CR200 Les Paul Style Electric Guitar",
+        price: 42000,
+        image: "https://cdn.shopify.com/s/files/1/0657/6821/files/cort-electric-guitars-black-cort-cr200-les-paul-style-electric-guitar-1178541370_540x.jpg?v=1768179533&width=1080"
+    },
+    {
+        id: 3,
+        name: "NS Design WAV4 Double Bass Violin Trans Red Gloss",
+        price: 114000,
+        image: "https://cdn.shopify.com/s/files/1/0657/6821/files/ns-design-violins-ns-design-wav4-violin-trans-red-gloss-1256507179032_540x.jpg?v=1688710535&width=1080"
+    },
+
+]
